425b59bb5b92bd8a8871cd5f6ad43bbb68179366
Fernando Herrera <faherrera2@gmail.com>
2022-03-11 13:14:00 +0100

    Feat: login, register y separar paginas sin autenticar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    ProgressComponent,
+    DashboardComponent,
+    NopagefoundComponent,
+    Grafica1Component,
+    LoginComponent,
+    RegisterComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
